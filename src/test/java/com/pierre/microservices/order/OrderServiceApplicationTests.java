4e0a1342efab06643e1f9bac0f727e78ef0dfd35
pierre enoc <pierre_enoc@yahoo.fr>
2025-09-09 17:09:49 -0300

    add integration test

--- src/test/java/com/pierre/microservices/order/OrderServiceApplicationTests.java
+++ src/test/java/com/pierre/microservices/order/OrderServiceApplicationTests.java
@@ -1,15 +1,53 @@
 package com.pierre.microservices.order;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
 
+	@ServiceConnection
+	static MySQLContainer mySQLContainerDBContainer = new MySQLContainer("mysql:8.3.0");
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+	static {
+		mySQLContainerDBContainer.start();
+		//System.setProperty("spring.data.mysql.uri", mySQLContainerDBContainer.getJdbcUrl());
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldSubmitOrder() {
+		String submitOrderJson = """
+				{
+				    "skuCode": "iphone_16",
+				    "price": 9999.99,
+				    "quantity": 1
+				}
+				""";
+		var responseBodyString = RestAssured.given()
+				.contentType("application/json")
+				.body(submitOrderJson)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.extract()
+				.body().asString();
+		assertThat(responseBodyString, org.hamcrest.Matchers.is("Order Placed Successfully"));
+		//assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
 	}
 
 }
