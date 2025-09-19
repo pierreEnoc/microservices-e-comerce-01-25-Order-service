50e46f1786cca66b320af47bc418c473d6124939
pierre enoc <pierre_enoc@yahoo.fr>
2025-09-19 09:15:51 -0300

    add client inventory

--- src/main/java/com/pierre/microservices/order/service/OrderService.java
+++ src/main/java/com/pierre/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.pierre.microservices.order.service;
 
+import com.pierre.microservices.order.client.InventoryClient;
 import com.pierre.microservices.order.dto.OrderRequest;
 import com.pierre.microservices.order.mdel.Order;
 import com.pierre.microservices.order.repository.OrderRepository;
@@ -13,13 +14,22 @@ import java.rmi.server.UID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-       Order order = new Order();
-       order.setOrderNumber(new UID().toString());
-       order.setPrice(orderRequest.price());
-       order.setQuantity(orderRequest.quantity());
-       order.setSkuCode(orderRequest.skuCode());
-         orderRepository.save(order);
+       var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+         if (isProductInStock) {
+             Order order = new Order();
+             order.setOrderNumber(new UID().toString());
+             order.setPrice(orderRequest.price());
+             order.setQuantity(orderRequest.quantity());
+             order.setSkuCode(orderRequest.skuCode());
+             orderRepository.save(order);
+         }else {
+             throw new RuntimeException("Product whith SkuCode " + orderRequest.skuCode() +"is not in stock, please try again later");
+
+         }
+
     }
 }
